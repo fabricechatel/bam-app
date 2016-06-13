737857c15b8f053a4003fb2a4085f45b97c752a5
djafasteve <djafasteve@yahoo.fr>
2016-06-13 10:19:16 +0200

    Correction des scripts de création de la base de données
    # Conflicts:
    #	src/config/database/createbase.sql
    #	src/config/database/testdata.sql

--- src/config/database/createbase.sql
+++ src/config/database/createbase.sql
@@ -1,22 +1,12 @@
-/*==============================================================*/
-/* Nom de SGBD :  MySQL 5.0                                     */
-/* Date de création :  09/06/2016 10:48:44                      */
-/*==============================================================*/
+drop schema bamdb;
+
 create schema if not exists bamdb;
 
 use bamdb;
 
 drop table if exists ADRESSE;
 
-drop table if exists APPARTENIR;
-
-drop table if exists APPLIQUER;
-
 drop table if exists ARTICLE;
-
-drop table if exists ASSOCIATION_17;
-
-drop table if exists ASSOCIATION_18;
 
 drop table if exists CARACTERISTIQUE;
 
@@ -28,25 +18,37 @@ drop table if exists COMMANDE;
 
 drop table if exists COMMENTAIRE;
 
-drop table if exists CONTIENT;
-
-drop table if exists DEPENDRE;
-
 drop table if exists FICHE;
+
+drop table if exists LIENS_CATEGORIE_ARTICLE;
+
+drop table if exists LIENS_CLIENT_ADRESSE;
+
+drop table if exists LIENS_COMMANDE_ADRESSE;
+
+drop table if exists LIENS_PANIER_ARTICLE;
+
+drop table if exists LIENS_PROMOTIONS_ARTICLES;
+
+drop table if exists LIENS_SPECS_ARTICLE;
+
+drop table if exists LIENS_SPECS_CATEGORIE;
+
+drop table if exists LIGNE_COMMANDE;
+
+drop table if exists LISTE_DE_SOUHAITS;
 
 drop table if exists MESSAGE;
 
 drop table if exists PANIER;
 
-drop table if exists POSSEDER;
-
 drop table if exists PROMOTION;
-
-drop table if exists REGROUPER;
 
 drop table if exists ROLE;
 
 drop table if exists UTILISATEUR;
+
+drop table if exists UTILISATEUR_ROLES;
 
 /*==============================================================*/
 /* Table : ADRESSE                                              */
@@ -62,59 +64,17 @@ create table ADRESSE
 );
 
 /*==============================================================*/
-/* Table : APPARTENIR                                           */
-/*==============================================================*/
-create table APPARTENIR
-(
-   ID_ARTICLE           int not null,
-   ID_CATEGORIE         int not null,
-   primary key (ID_ARTICLE, ID_CATEGORIE)
-);
-
-/*==============================================================*/
-/* Table : APPLIQUER                                            */
-/*==============================================================*/
-create table APPLIQUER
-(
-   ID_ARTICLE           int not null,
-   ID_PROMOTION         int not null,
-   primary key (ID_ARTICLE, ID_PROMOTION)
-);
-
-/*==============================================================*/
 /* Table : ARTICLE                                              */
 /*==============================================================*/
 create table ARTICLE
 (
    ID_ARTICLE           int not null,
-   REF_ARTICLE          varchar(64) not null,
-   PRIX_ARTICLE         decimal not null,
-   QUANTITE_ARTICLE     int not null,
-   IS_SALABLE           bool not null,
+   LIBELLE              varchar(128),
+   REFARTICLE           varchar(64) not null,
+   PRIX                 decimal not null,
+   QUANTITESTOCK        int not null,
+   VISIBLE              bool,
    primary key (ID_ARTICLE)
-);
-
-/*==============================================================*/
-/* Table : ASSOCIATION_17                                       */
-/*==============================================================*/
-create table ASSOCIATION_17
-(
-   ID_ARTICLE           int not null,
-   ID_CLIENT            int not null,
-   primary key (ID_ARTICLE, ID_CLIENT)
-);
-
-/*==============================================================*/
-/* Table : ASSOCIATION_18                                       */
-/*==============================================================*/
-create table ASSOCIATION_18
-(
-   ID_COMMANDE          int not null,
-   ID_ARTICLE           int not null,
-   QUANTITE_ARTICLE     int,
-   PRIX_ARTICLE         decimal,
-   STATUT               varchar(16),
-   primary key (ID_COMMANDE, ID_ARTICLE)
 );
 
 /*==============================================================*/
@@ -123,7 +83,8 @@ create table ASSOCIATION_18
 create table CARACTERISTIQUE
 (
    ID_CARACTERISTIQUE   int not null,
-   LIBELLE_CARACTERISTIQUE varchar(128) not null,
+   ATTRIBUT             varchar(128),
+   VALEUR               varchar(128) not null,
    primary key (ID_CARACTERISTIQUE)
 );
 
@@ -134,7 +95,8 @@ create table CATEGORIE
 (
    ID_CATEGORIE         int not null,
    IDPARENT             int not null,
-   LIBELLE_CATEGORIE    varchar(128) not null,
+   LIBELLE_CATEGORIE    varchar(128),
+   ACTIVE               bool,
    primary key (ID_CATEGORIE)
 );
 
@@ -144,13 +106,12 @@ create table CATEGORIE
 create table CLIENT
 (
    ID_CLIENT            int not null,
-   ID_PANIER            int,
    ID_UTILISATEUR       int not null,
    EMAIL                varchar(128) not null,
    NOM                  varchar(64) not null,
    PRENOM               varchar(64) not null,
    CIVILITE             varchar(8) not null,
-   STATUT_CLIENT        bool not null,
+   ACTIF                bool,
    primary key (ID_CLIENT)
 );
 
@@ -176,31 +137,10 @@ create table COMMENTAIRE
    ID_COMMENTAIRE       int not null,
    ID_ARTICLE           int not null,
    ID_CLIENT            int not null,
-   NOTE                 decimal not null,
-   DATE_COMMENTAIRE     datetime not null,
-   IS_APPROVED          bool not null,
+   NOTE                 decimal,
+   DATE                 datetime,
+   VISIBLE              bool,
    primary key (ID_COMMENTAIRE)
-);
-
-/*==============================================================*/
-/* Table : CONTIENT                                             */
-/*==============================================================*/
-create table CONTIENT
-(
-   ID_PANIER            int not null,
-   ID_ARTICLE           int not null,
-   QUANTITE_ARTICLE     int,
-   primary key (ID_PANIER, ID_ARTICLE)
-);
-
-/*==============================================================*/
-/* Table : DEPENDRE                                             */
-/*==============================================================*/
-create table DEPENDRE
-(
-   ID_CARACTERISTIQUE   int not null,
-   ID_CATEGORIE         int not null,
-   primary key (ID_CARACTERISTIQUE, ID_CATEGORIE)
 );
 
 /*==============================================================*/
@@ -210,11 +150,107 @@ create table FICHE
 (
    ID_FICHE             int not null,
    ID_ARTICLE           int not null,
-   DESCRIPTION          varchar(1024) not null,
+   NOM                  varchar(64),
    REFFICHE             varchar(32),
+   DESCRIPTION          varchar(1024) not null,
    IMAGE                varchar(256),
    IS_PUBLISHED         bool,
    primary key (ID_FICHE)
+);
+
+/*==============================================================*/
+/* Table : LIENS_CATEGORIE_ARTICLE                              */
+/*==============================================================*/
+create table LIENS_CATEGORIE_ARTICLE
+(
+   ID_ARTICLE           int not null,
+   ID_CATEGORIE         int not null,
+   primary key (ID_ARTICLE, ID_CATEGORIE)
+);
+
+/*==============================================================*/
+/* Table : LIENS_CLIENT_ADRESSE                                 */
+/*==============================================================*/
+create table LIENS_CLIENT_ADRESSE
+(
+   ID_CLIENT            int not null,
+   ID_ADRESSE           int not null,
+   primary key (ID_CLIENT, ID_ADRESSE)
+);
+
+/*==============================================================*/
+/* Table : LIENS_COMMANDE_ADRESSE                               */
+/*==============================================================*/
+create table LIENS_COMMANDE_ADRESSE
+(
+   ID_COMMANDE          int not null,
+   ID_ADRESSE           int not null,
+   ISFACTURATION        bool,
+   primary key (ID_COMMANDE, ID_ADRESSE)
+);
+
+/*==============================================================*/
+/* Table : LIENS_PANIER_ARTICLE                                 */
+/*==============================================================*/
+create table LIENS_PANIER_ARTICLE
+(
+   IDPANIER             int not null,
+   ID_ARTICLE           int not null,
+   QUANTITEPANIER       int,
+   primary key (IDPANIER, ID_ARTICLE)
+);
+
+/*==============================================================*/
+/* Table : LIENS_PROMOTIONS_ARTICLES                            */
+/*==============================================================*/
+create table LIENS_PROMOTIONS_ARTICLES
+(
+   ID_ARTICLE           int not null,
+   ID_PROMOTION         int not null,
+   primary key (ID_ARTICLE, ID_PROMOTION)
+);
+
+/*==============================================================*/
+/* Table : LIENS_SPECS_ARTICLE                                  */
+/*==============================================================*/
+create table LIENS_SPECS_ARTICLE
+(
+   ID_CARACTERISTIQUE   int not null,
+   ID_ARTICLE           int not null,
+   primary key (ID_CARACTERISTIQUE, ID_ARTICLE)
+);
+
+/*==============================================================*/
+/* Table : LIENS_SPECS_CATEGORIE                                */
+/*==============================================================*/
+create table LIENS_SPECS_CATEGORIE
+(
+   ID_CARACTERISTIQUE   int not null,
+   ID_CATEGORIE         int not null,
+   primary key (ID_CARACTERISTIQUE, ID_CATEGORIE)
+);
+
+/*==============================================================*/
+/* Table : LIGNE_COMMANDE                                       */
+/*==============================================================*/
+create table LIGNE_COMMANDE
+(
+   ID_COMMANDE          int not null,
+   ID_ARTICLE           int not null,
+   QUANTITECOMMANDE     int,
+   PRIX                 decimal,
+   STATUT               varchar(16),
+   primary key (ID_COMMANDE, ID_ARTICLE)
+);
+
+/*==============================================================*/
+/* Table : LISTE_DE_SOUHAITS                                    */
+/*==============================================================*/
+create table LISTE_DE_SOUHAITS
+(
+   ID_ARTICLE           int not null,
+   ID_CLIENT            int not null,
+   primary key (ID_ARTICLE, ID_CLIENT)
 );
 
 /*==============================================================*/
@@ -224,11 +260,11 @@ create table MESSAGE
 (
    ID_MESSAGE           int not null,
    ID_SENDER            int not null,
-   ID_RECIEVER          int not null,
+   ID_RECEIVER          int not null,
    INTITULE             varchar(64) not null,
    CORPS_MESSAGE        varchar(1024) not null,
-   DATE_MESSAGE         date not null,
-   STATUT_MESSAGE       bool not null,
+   DATE_MESSAGE         datetime not null,
+   LU                   bool,
    primary key (ID_MESSAGE)
 );
 
@@ -237,23 +273,10 @@ create table MESSAGE
 /*==============================================================*/
 create table PANIER
 (
-   ID_PANIER            int not null,
+   IDPANIER             int not null,
    ID_CLIENT            int,
-   REF_INTERNAUTE       varchar(64) not null,
-   IS_ACTIVE_PANIER     bool not null,
-   primary key (ID_PANIER)
-);
-
-/*==============================================================*/
-/* Table : POSSEDER                                             */
-/*==============================================================*/
-create table POSSEDER
-(
-   ID_CLIENT            int not null,
-   ID_ADRESSE           int not null,
-   IS_POSTAL            bool,
-   IS_DELIVERY          bool,
-   primary key (ID_CLIENT, ID_ADRESSE)
+   REFINTERNAUTE        varchar(64) not null,
+   primary key (IDPANIER)
 );
 
 /*==============================================================*/
@@ -262,19 +285,10 @@ create table POSSEDER
 create table PROMOTION
 (
    ID_PROMOTION         int not null,
+   NOM                  varchar(64),
    POURCENTAGE          int not null,
-   IS_ACTIVE            bool not null,
+   ACTIVE               bool,
    primary key (ID_PROMOTION)
-);
-
-/*==============================================================*/
-/* Table : REGROUPER                                            */
-/*==============================================================*/
-create table REGROUPER
-(
-   ID_CARACTERISTIQUE   int not null,
-   ID_ARTICLE           int not null,
-   primary key (ID_CARACTERISTIQUE, ID_ARTICLE)
 );
 
 /*==============================================================*/
@@ -282,9 +296,9 @@ create table REGROUPER
 /*==============================================================*/
 create table ROLE
 (
-   ID_ROLE              int not null,
-   NOM                  varchar(64) not null,
-   primary key (ID_ROLE)
+   NOMROLE              varchar(64) not null,
+   ENABLED              bool not null,
+   primary key (NOMROLE)
 );
 
 /*==============================================================*/
@@ -293,10 +307,20 @@ create table ROLE
 create table UTILISATEUR
 (
    ID_UTILISATEUR       int not null,
-   ID_CLIENT            int,
-   ID_ROLE              int not null,
    LOGIN                varchar(64) not null,
    MDP                  varchar(64) not null,
+   ACTIF                bool,
    primary key (ID_UTILISATEUR)
 );
 
+/*==============================================================*/
+/* Table : UTILISATEUR_ROLES                                    */
+/*==============================================================*/
+create table UTILISATEUR_ROLES
+(
+   IDUTILISATEURROLE    int not null,
+   NOMROLE              varchar(64) not null,
+   ID_UTILISATEUR       int not null,
+   ENABLED              bool not null,
+   primary key (IDUTILISATEURROLE)
+);
