0de0fd6f6434b5c33a9c69cdf434083296f90899
hans <dhanshans@yahoo.com>
2016-06-15 15:49:25 +0200

    panierDao

--- bam-services-impl/src/main/java/com/bam/dao/PanierDao.java
+++ bam-services-impl/src/main/java/com/bam/dao/PanierDao.java
@@ -3,5 +3,8 @@ package com.bam.dao;
 import com.bam.entity.Panier;
 
 public interface PanierDao extends GenericDao<Panier, Integer>{
+	
+	public Panier getPanierByClientId(int idClient);
+	public Panier getPanierByCookie(String str);
 
 }
